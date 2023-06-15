112f7836dacebfa94d1e7fca6ad7c2c33b61b86d
AjmalRasouli <88502375+ajmalrasouli@users.noreply.github.com>
2023-06-15 22:16:29 +0100

    first commit

---
+++ src/services/listService.js
@@ -0,0 +1,21 @@
+/**
+ * Retrive sample data
+ * @returns data for list widget
+ */
+export const getListData = () => [
+  {
+    id: "id1",
+    title: "See your weekly summary",
+    content: "Your Weekly Summary describes your weekly fitness activity, which includes Calories, Avg. Calories, Steps, Distance",
+  },
+  {
+    id: "id2",
+    title: "Change your goals",
+    content: "This allows you to work out a plan to achieve your goal by breaking it into daily actions",
+  },
+  {
+    id: "id3",
+    title: "Check your activity history",
+    content: "Achieve your fitness goals through customised coaching and actionable tips based on your health and activity history",
+  },
+];
